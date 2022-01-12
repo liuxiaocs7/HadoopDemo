7c7b0b23ec1373d4ae3fc44a636755fe057adbe4
liuxiao <liuxiao2103@qq.com>
2022-01-12 22:08:14 +0800

    feat: combiner基本使用

---
+++ MapReduceDemo/src/main/java/com/liuxiaocs/mapreduce/combiner/WordCountReducer.java
@@ -0,0 +1,31 @@
+package com.liuxiaocs.mapreduce.combiner;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * KEYIN: reduce阶段输入的key的类型：Text
+ * VALUEIN: reduce阶段输入value的类型 IntWritable
+ * KEYOUT: reduce阶段输出的key类型 Text
+ * VALUEOUT: reduce阶段输出的value类型 IntWritable
+ */
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    private static IntWritable outV = new IntWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        // liuxiaocs (1, 1)
+        // 累加
+        for(IntWritable value : values) {
+            sum += value.get();
+        }
+        // 写出
+        outV.set(sum);
+        context.write(key, outV);
+    }
+}
